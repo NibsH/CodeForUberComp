681ad6b693f64dd6bc6dd70638d2d49f5e8eac11
NibsH <133595813+NibsH@users.noreply.github.com>
2023-09-20 19:28:08 -0400

    Update mapforbustops.py

--- mapforbustops.py
+++ mapforbustops.py
@@ -9,4 +9,4 @@ barcelona_map = folium.Map(location=[41.3851, 2.1734], zoom_start=12)
 folium.GeoJson(gdf).add_to(barcelona_map)
 
 
-barcelona_map.save(r"C:\Users\nabal\Downloads\barcelona_districts.html")
+barcelona_map.save(r"barcelona_districts.html")
